7450b78365a823a2e4d786646b27f1add5b49006
David Wilker <davidwilker2510@gmail.com>
2021-08-26 02:02:14 -0300

    Fazendo os primeiros testes de movimentacao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,15 +8,20 @@ namespace xadrez_console {
 
 
             try {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                while (!partida.teminada) {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
 
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
-                Tela.imprimirTabuleiro(tab);
+                    partida.executaMovimento(origem, destino);
+                }                
             }
             catch (TabuleiroException e) {
                 Console.WriteLine(e.Message);
--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -7,14 +7,18 @@ namespace tabuleiro {
 
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
-        public int qdtMovimentos { get; protected set; }
+        public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; set; }
 
         public Peca(Tabuleiro tab, Cor cor) {
             this.posicao = null;
             this.tab = tab;
             this.cor = cor;
-            this.qdtMovimentos = 0;
+            this.qteMovimentos = 0;
+        }
+
+        public void incrementarQteMovimentos() {
+            qteMovimentos++;
         }
     }
 }
